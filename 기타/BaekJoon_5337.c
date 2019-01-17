bb3a1364a5cc639a430b39faf3c2149976a9dc7e
JJangSungWon <dnjs2113@gmail.com>
2019-01-17 11:47:17 +0900

    기타 / BaekJoon_5337

---
+++ 기타/BaekJoon_5337.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+//JJangSungWon BaekJoon_5337
+int main(void)
+{
+	puts(".  .   .");
+	puts("|  | _ | _. _ ._ _  _");
+	puts("|/\\|(/.|(_.(_)[ | )(/.");
+
+	system("pause");
+	return 0;
+}
