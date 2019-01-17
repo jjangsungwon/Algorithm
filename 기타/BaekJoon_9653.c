0c00080017aefdd65497363e4eb5ddd657458963
JJangSungWon <dnjs2113@gmail.com>
2019-01-17 11:02:17 +0900

    기타 / BaekJoon_9653

---
+++ 기타/BaekJoon_9653.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+//JJangSungWon BaekJoon_9653
+int main(void)
+{
+	puts("    8888888888  888    88888");
+	puts("   88     88   88 88   88  88");
+	puts("    8888  88  88   88  88888");
+	puts("       88 88 888888888 88   88");
+	puts("88888888  88 88     88 88    888888");
+	puts("");
+	puts("88  88  88   888    88888    888888");
+	puts("88  88  88  88 88   88  88  88");
+	puts("88 8888 88 88   88  88888    8888");
+	puts(" 888  888 888888888 88  88      88");
+	puts("  88  88  88     88 88   88888888");
+
+	//system("pause");
+	return 0;
+}
