d602863e9e3e49eb3cc7543b14180c47db445e35
JJangSungWon <dnjs2113@gmail.com>
2019-02-02 23:46:05 +0900

    기타 / BaekJoon10172

---
+++ 기타/BaekJoon10172.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+
+//BaekJoon_10172
+
+int main(void)
+{
+	puts("|\\_/|");
+	puts("|q p|   /}");
+	puts("( 0 )\"\"\"\\");
+	puts("|\"^\"`    |");
+	puts("||_/=\\\\__|");
+
+	return 0;
+}
