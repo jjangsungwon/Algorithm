352c985b4dca085ef2ec34ef737bc9a52924254c
장성원 <dnjs2113@gmail.com>
2020-02-16 12:17:31 +0900

    블랙잭 - dfs를 이용하여 완전 탐색을 하였다

--- BAEKJOON_2798.py
+++ BAEKJOON_2798.py
@@ -1,43 +1,25 @@
 N, M = map(int, input().split())
-card = list(map(int, input().split()))
+cards = list(map(int, input().split()))
 
-result = []
-real_sum = 1
+answer = 0
 
-# 첫 번째 숫자를 넣는다
-result.append(card[0])
-count = 1
-idx = 1
-real_sum = 0
 
-if len(card) == 3:
-    print(sum(card))
-    exit()
-else:
-    for i in range(N - 2):
-        result.clear()
-        result.append(card[i])
-        idx = i + 1
-        check = 2
-        while idx <= len(card):
-            if len(result) == 3:
-                if sum(result) == M:
-                    real_sum = sum(result)
-                    print(real_sum)
-                    exit()
-                else:
-                    if real_sum < sum(result) <= M:
-                        real_sum = sum(result)
+def dfs(n, idx, cards_sum):
+    if idx == 3:
+        global answer
+        if answer < cards_sum <= M:
+            answer = cards_sum
+        return
 
-                    if idx == len(card)-1:
-                        result.clear()
-                        result.append(card[i])
-                        idx = i + check
-                        check = check + 1
-                    else:
-                        result.pop()
-            else:
-                if idx < len(card):
-                    result.append(card[idx])
-                idx += 1
-    print(real_sum)
+    if n >= len(cards):
+        return
+
+    # 현재 카드를 선택하고 내가 가진 카드 중 다음 카드로 넘어간다.
+    dfs(n + 1, idx + 1, cards_sum + cards[n])
+
+    # 현재 카드를 선택하지 않고 다음 카드로 넘어간다.
+    dfs(n + 1, idx, cards_sum)
+
+
+dfs(0, 0, 0)
+print(answer)
