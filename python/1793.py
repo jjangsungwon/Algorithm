41434dcd8ea0ed7ab98e2d533ee27ab2bc45c6bb
장성원 <dnjs2113@gmail.com>
2020-02-22 16:39:59 +0900

    타일링 - 2 * N 타일 문제와 동일하게 dp로 풀었지만 입력의 개수가 제한되어 있지 않는 부분이 힘들었다. - try except을 이용해서 해결하였다.

---
+++ python/1793.py
@@ -0,0 +1,29 @@
+#github blog : jjangsungwon.github.io
+#baekjoon : 1793
+import sys
+
+sys.setrecursionlimit(10**8)
+
+d = [0] * 1001
+
+def dp(n):
+    if n == 0:
+        return 1
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 3
+    else:
+        if d[n] != 0:
+            return d[n] 
+        else:
+            d[n] = dp(n-1) + 2 * dp(n-2) 
+            return d[n] 
+
+
+while True:
+    try :
+        N = int(sys.stdin.readline())
+        print(dp(N))
+    except :
+        break
