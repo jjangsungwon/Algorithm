fadb59f049b240f75d943909684c56b7c7416ead
장성원 <dnjs2113@gmail.com>
2020-02-23 20:28:47 +0900

    1, 2, 3 더하기 - dp를 이용하여 해결하였습니다.

---
+++ python/9095.py
@@ -0,0 +1,29 @@
+# github blog : jjangsungwon.github.io
+# baekjoon : 9095
+import sys
+
+T = int(sys.stdin.readline())
+data = []
+for i in range(T):
+    data.append(int(sys.stdin.readline()))
+
+d = [0 for _ in range(100)]
+
+
+def dp(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    else:
+        if d[n] != 0:
+            return d[n]
+        else:
+            d[n] = dp(n - 1) + dp(n - 2) + dp(n - 3)
+            return d[n]
+
+
+for i in range(T):
+    print(dp(data[i]))
