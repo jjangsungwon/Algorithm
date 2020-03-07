074c4f0398ccd872af955095eefa349d5dbe309a
장성원 <dnjs2113@gmail.com>
2020-03-07 11:57:02 +0900

    주사위 - 단순 구현

---
+++ python/9295.py
@@ -0,0 +1,10 @@
+import sys
+
+if __name__ == "__main__":
+    
+    T = int(input())
+    
+    for i in range(1, T + 1):
+        a, b = map(int, input().split())
+        
+        print("Case {0}: {1}".format(i, a+b))
