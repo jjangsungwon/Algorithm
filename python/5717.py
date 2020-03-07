aed694cd0cc980da95ac9e985ad21d530509702e
장성원 <dnjs2113@gmail.com>
2020-03-07 13:34:51 +0900

    상근이의 친구들 - 단순 구현

---
+++ python/5717.py
@@ -0,0 +1,11 @@
+import sys
+
+if __name__ == "__main__":
+    
+    while True:
+        M, F = map(int, input().split())
+    
+        if M == 0 and F == 0:
+            break
+        else:
+            print(M + F)
