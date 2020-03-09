5943da0a5fe69917fd822c3444c37b2258b5676d
장성원 <dnjs2113@gmail.com>
2020-03-09 19:09:23 +0900

    지능형 기차2 - 단순 구현

---
+++ python/2460.py
@@ -0,0 +1,18 @@
+
+if __name__ == "__main__":
+    data = []
+    
+    for i in range(10):
+        data_list = list(map(int, input().split()))
+        data.append(data_list)
+        
+    max_people = 0
+    people = 0
+    for i in range(10):
+        people -= data[i][0]
+        people += data[i][1]
+        
+        if people > max_people:
+            max_people = people
+    
+    print(max_people)
