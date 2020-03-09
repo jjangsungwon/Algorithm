03d20f8f6cbc0f52c66e4320b403a6e36d0efa7e
장성원 <dnjs2113@gmail.com>
2020-03-09 18:45:17 +0900

    주사위 세개 - count 활용

--- python/2480.py
+++ python/2480.py
@@ -1,27 +1,21 @@
-
 if __name__ == "__main__":
-    
+
     data_list = list(map(int, input().split()))
-    
+
     max_count = 0
     index = 0
-    
-    for i in range(3):
-        count = -1
-        for j in range(3):
-            if data_list[i] == data_list[j]:
-                count += 1            
-        if max_count < count:
-            index = i
-            max_count = count
-    
-    value = 9
-    if max_count == 0:
+
+    for data in data_list:
+        if max_count < data_list.count(data):
+            max_count = data_list.count(data)
+            index = data_list.index(data)
+
+    value = 0
+    if max_count == 1:
         value = max(data_list) * 100
-    elif max_count == 1:
+    elif max_count == 2:
         value = 1000 + (data_list[index]) * 100
     else:
         value = 10000 + (data_list[index]) * 1000
-    
+
     print(value)
-    
