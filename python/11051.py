27f7f8e3f8710a30d20697a470f3053d854c2ea0
장성원 <dnjs2113@gmail.com>
2020-03-10 16:57:25 +0900

    이항 계수 2 - 2차원 dp 활용

---
+++ python/11051.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    
+    n, k = map(int, input().split())
+    
+    dp = [[1] for _ in range(n + 1)]
+    
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            if j == i:
+                dp[i].append(1)
+            else:
+                dp[i].append((dp[i-1][j-1] + dp[i-1][j]) % 10007)
+                
+    print(dp[n][k])
+    
+    
