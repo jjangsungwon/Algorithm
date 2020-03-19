864b7b5d32550ee5fb18c29842c47b50d8094763
장성원 <dnjs2113@gmail.com>
2020-03-19 20:56:47 +0900

    사분면 고르기 - 조건문

---
+++ C/14681.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int x, y;
+    
+    scanf("%d", &x);
+    scanf("%d", &y);
+
+    if (x > 0)
+    {
+        if (y > 0)
+            printf("1");
+        else
+            printf("4");
+    }
+    else
+    {
+        if (y > 0)
+            printf("2");
+        else
+            printf("3");
+    }
+
+    return 0;
+}
