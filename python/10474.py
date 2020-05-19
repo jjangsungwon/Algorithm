183776d4424f6e22186a278200e4d20f642583ba
장성원 <dnjs2113@gmail.com>
2020-05-20 03:36:44 +0900

    분수좋아해? - 단순 구현(반복문, 조건문, 나눗셈) - 난이도 브론즈 3

---
+++ python/10474.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+
+    while True:
+        a, b = map(int, input().split())
+        if a == 0 and b == 0:
+            break
+
+        quotient = a // b
+        print(quotient, a - quotient * b, "/", b)
