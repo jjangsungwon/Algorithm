f7b7fd1e7123c512d8f0783c5bc53848af558b87
JJangSungWon <dnjs2113@gmail.com>
2021-01-10 15:16:46 +0900

    문자열 내 마음대로 정렬하기
    [사용한 알고리즘]
    정렬
    
    [문제 접근]
    기존의 문자열을 n 번째 문자 + 문자열로 바꾼 후 정렬을 하였습니다.
    n 번째 문자가 제일 앞이기 때문에 정렬을 수행하면  n 번째 문자를 기준으로 정렬이 됩니다.
    
    [알고리즘]
    1. 기존의 문자열을 n 번째 문자 + 문자열로 바꿉니다.
    2. 오름차순으로 정렬한 후 리턴합니다.

---
+++ Programmers/Java/level 1/문자열내마음대로정렬하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        String[] answer = new String[strings.length];
+        
+        for(int i=0; i<strings.length; i++){
+            strings[i] = strings[i].charAt(n) + strings[i];
+        }
+        Arrays.sort(strings);
+        for(int i=0; i<strings.length; i++){
+            answer[i] = strings[i].substring(1, strings[i].length());
+        }
+        
+        return answer;
+    }
+}
