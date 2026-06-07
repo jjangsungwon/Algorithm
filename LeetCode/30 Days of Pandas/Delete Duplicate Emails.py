f1ee46316b1ac38677450f2db2a603bee4df7d6c
Sungwon Jang <dnjs2113@gmail.com>
2026-06-07 17:44:22 +0900

    [LeetCode] Delete Duplicate Emails

---
+++ LeetCode/30 Days of Pandas/Delete Duplicate Emails.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
+    scores = scores.sort_values(by='score', ascending=False)
+    scores['rank'] = scores['score'].rank(method='dense', ascending=False)
+    return scores[['score', 'rank']]
+
+
+if __name__ == "__main__":
+    print(order_scores(scores))
+    
